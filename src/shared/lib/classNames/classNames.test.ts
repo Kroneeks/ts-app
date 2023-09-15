c2dcd11920bfa37554afb7f8351aca94d40509be
Sasha Donets <sashadoneze@gmail.com>
2023-09-15 14:03:56 +0300

    Install jest, add first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+  test('With one param', () => {
+    expect(classNames('someclass')).toBe('someclass')
+  })
+
+  test('With additional param', () => {
+    expect(classNames('someclass', {}, ['class1'])).toBe('someclass class1')
+  })
+
+  test('With mods', () => {
+    expect(classNames('someclass', { hovered: true }, ['class1'])).toBe('someclass class1 hovered')
+  })
+
+  test('With mods false', () => {
+    expect(classNames('someclass', { hovered: true, scrollable: false }, ['class1'])).toBe('someclass class1 hovered')
+  })
+})
