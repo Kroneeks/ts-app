182b4376744553a097350ab703b8838cf29a1cb8
Sasha Donets <sashadoneze@gmail.com>
2023-10-22 20:31:25 +0300

    Add reg-cli reports

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
 
 import { Sidebar } from './Sidebar'
 
 const meta = {
   title: 'widgets/Sidebar',
   component: Sidebar,
+  decorators: [
+    (Story) => (
+        <BrowserRouter>
+            <Story />
+        </BrowserRouter>
+    )
+  ],
   parameters: {
     layout: 'centered'
   },
