78df9ad20813364d1af4e55013ca999a164adae4
Sasha Donets <sashadoneze@gmail.com>
2023-11-01 17:45:49 +0300

    Add pre commit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import cls from './LoginModal.module.scss'
 import { LoginForm } from '../LoginForm/LoginForm'
 import { Modal } from 'shared/ui/Modal/Modal'
 import { useTheme } from 'app/providers/ThemeProvider'
@@ -14,7 +13,7 @@ const LoginModal = ({ className = '', isOpen, onClose }: LoginModalProps) => {
   const { theme, toggleTheme } = useTheme()
 
   return (
-      <Modal className={classNames(`${cls.LoginModal} modal`, {}, [className, theme])} isOpen={isOpen} onClose={onClose}>
+      <Modal className={classNames('modal', {}, [className, theme])} isOpen={isOpen} onClose={onClose}>
           <LoginForm />
       </Modal>
   )
