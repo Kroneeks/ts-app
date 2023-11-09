f132965711d4662d93273599e16124f63b98c397
Sasha Donets <sashadoneze@gmail.com>
2023-11-09 20:18:11 +0300

    Add articles page

---
+++ src/pages/ArticlePage/ui/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,22 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './ArticlePage.module.scss'
+import { useTranslation } from 'react-i18next'
+import { memo } from 'react'
+
+interface ArticlePageProps {
+  className?: string
+}
+
+const ArticlePage = memo(({ className = '' }: ArticlePageProps) => {
+  const { t } = useTranslation()
+
+  return (
+      <div className={classNames(cls.ArticlePage, {}, [className])}>
+          ARTICLE PAGE
+      </div>
+  )
+})
+
+ArticlePage.displayName = 'ArticlePage'
+
+export default ArticlePage
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,4 +1,6 @@
 import { AboutPage } from 'pages/AboutPage'
+import { ArticleDetailsPage } from 'pages/ArticleDetailsPage'
+import { ArticlePage } from 'pages/ArticlePage'
 import { MainPage } from 'pages/MainPage'
 import { NotFoundPage } from 'pages/NotFoundPage'
 import { ProfilePage } from 'pages/ProfilePage'
@@ -12,6 +14,8 @@ export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
   PROFILE = 'profile',
+  ARTICLES = 'articles',
+  ARTICLE_DETAILS = 'articleDetails',
   NOT_FOUND = 'notfound',
 }
 
@@ -19,6 +23,8 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
   [AppRoutes.PROFILE]: '/profile',
+  [AppRoutes.ARTICLES]: '/articles',
+  [AppRoutes.ARTICLE_DETAILS]: '/articles/',
   [AppRoutes.NOT_FOUND]: '*'
 }
 
@@ -34,6 +40,16 @@ export const routeConfig: Record<AppRoutes, AppRouteProps> = {
   [AppRoutes.PROFILE]: {
     path: RoutePath.profile,
     element: <ProfilePage />,
+    authOnly: true
+  },
+  [AppRoutes.ARTICLES]: {
+    path: RoutePath.articles,
+    element: <ArticlePage />,
+    authOnly: true
+  },
+  [AppRoutes.ARTICLE_DETAILS]: {
+    path: `${RoutePath.articleDetails}:id`,
+    element: <ArticleDetailsPage />,
     authOnly: true
   },
   [AppRoutes.NOT_FOUND]: {
